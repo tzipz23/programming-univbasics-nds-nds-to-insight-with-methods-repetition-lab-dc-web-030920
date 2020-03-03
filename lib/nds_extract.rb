ce22b3abb267b3f19da05209db3026e22fb3da63
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-03 16:33:45 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,10 +25,29 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
+  
+  director_index = 0
+  director_list = Array.new
+
+  while director_index < source.size do
+    director_list.push(source[director_index][:name])
+    director_index += 1
+  end
+  director_list
   # Write this implementation
 end
 
 def total_gross(source)
+  
+  total_gross_of_earnings = 0
+  director_index = 0
+
+  while director_index < source.size do
+    total_gross_of_earnings += gross_for_director( source[director_index] )
+    director_index += 1
+  end
+total_gross_of_earnings
+  
   # Write this implementation
   #
   # Should use methods:
